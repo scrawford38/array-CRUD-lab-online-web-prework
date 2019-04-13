233a4b922731f6fc712252df20e54a65a90dfe4c
Sally Crawford <sally.crawford@gmail.com>
2019-04-13 13:47:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   new_array=array.unshift(element)
-  return new_aray
+  return new_array
 end
 
 def remove_element_from_end_of_array(array)
