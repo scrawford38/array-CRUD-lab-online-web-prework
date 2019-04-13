e73eab7bae6c18db7da33f95602678ca8de0ae95
Sally Crawford <sally.crawford@gmail.com>
2019-04-13 13:55:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,18 +7,15 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array=array << element
-  return new_array
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  new_array=array.unshift(element)
-  return new_array
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  new_array=array.pop
-  return new_array
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
