233689a03455353bcdcf79d49f38cc8f7a857f98
Sally Crawford <sally.crawford@gmail.com>
2019-04-13 13:49:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  new_aray=aray.shift
+  return new_array
 end
 
 def retrieve_element_from_index(array, index_number)
